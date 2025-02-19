ccf92ce5707a15eea52b2661e2c9f73991d2ab2b
RustyGearBox <gearbox.bit@gmail.com>
2025-02-19 13:53:58 +0100

    swagger

--- pokemon/src/main/java/edu/api/pokemon/Controller/PokemonController.java
+++ pokemon/src/main/java/edu/api/pokemon/Controller/PokemonController.java
@@ -25,13 +25,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-
 @RestController
 @RequestMapping("/pokemon")
 @RequiredArgsConstructor
 public class PokemonController {
-    
+
     private final PokemonService pokemonService;
     private final UpdateService updateService;
 
